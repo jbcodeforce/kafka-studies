faf12d1b68b02c8d298c7508bf94414d66f3efdb
Jerome Boyer <boyerje@us.ibm.com>
2020-03-19 07:44:49 -0700

    add some properties for topic creation

--- mirror-maker-2/es-cluster/createMMtopics.sh
+++ mirror-maker-2/es-cluster/createMMtopics.sh
@@ -1,2 +1,2 @@
 /opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka1:9092 --list
-/opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka1:9092 --create  --replication-factor 1 --partitions 1 --topic source.orders
+/opt/kafka/bin/kafka-topics.sh --bootstrap-server kafka1:9092 --create  --replication-factor 1 --partitions 1 --topic source.products
